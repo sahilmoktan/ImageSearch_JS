937e11375a03d35fac77523a87078be7c66b2f03
Sahil Moktan <moktanshail9@gmail.com>
2023-06-14 23:52:27 +0545

    image fetch successfully now show more buttion work left

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let page = 1;
 
 async function searchImages() {
   inputData = inputEl.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`;
+  const url = `https://api.unsplash.com/search/photos/?page=${page}&query=${inputData}&client_id=${accesskey}`;
   const response = await fetch(url);
   const data = await response.json();
 
@@ -19,16 +19,16 @@ async function searchImages() {
     searchReasults.innerHTML = "";
   }
 
-  results.map((result) => {
+  results.map((results) => {
     const imageWrapper = document.createElement("div");
     imageWrapper.classList.add("search-result");
     const image = document.createElement("img");
-    image.src = response.urls.thumbnails;
-    image.alt = result.alt_description;
+    image.src = results.urls.small;
+    image.alt = results.alt_description;
     const imageLink = document.createElement("a");
-    imageLink.href = result.links.innerHTML;
+    imageLink.href = results.links.innerHTML;
     imageLink.target = "_blank";
-    imageLink.textContent = result.alt_description;
+    imageLink.textContent = results.alt_description;
 
     imageWrapper.appendChild(image);
     imageWrapper.appendChild(imageLink);
